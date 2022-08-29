9da671bdf13dd0263294708d0ffdfe9d16a8b884
Yordi70 <condoriyordi75@gmail.com>
2022-08-29 10:51:14 -0400

    add message bot test

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/webhook',(req, res) => {
+    console.log('post: webhook');
+
+    const body = req.body;
+    if(body.object === 'page'){
+        body.forEach(entry => {
+            //se reciben los mensajes
+            const webhookEvent = entry.messaging[0];
+            console.log(webhookEvent);
+            res.status(200).send('Evento Recibido');
+        });
+    }else {
+        res.sendStatus(404);
+    }
+});
+app.get('/webhook',(req, res) => {
+    console.log('get: webhook');
+    const VERIFY_TOKEN = 'my_verify_token_unique';
+    
+    const mode = req.query['hub.mode'];
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
+
+    if(mode && token){
+        if(mode === 'subscribe' && token === VERIFY_TOKEN){
+            console.log('WebHook Verificado');
+            res.status(200).send(challenge);
+        }else{
+            res.sendStatus(404);
+        }
+    }else {
+        res.sendStatus(404);
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Servidor iniciado');
+});
